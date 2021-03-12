88cd469854299cd397755d43690bb4eb37a64fd3
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-03-12 12:23:04 +0600

    Added JSON Placeholder Datas
    added data and installed axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './Root.css'
+import axios from 'axios'
 
-const Home =()=>{
-    return(
-      <div className="post card center">
+class Home extends Component {
+
+  state = {
+    posts: []
+  }
+
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      })
+  }
+
+  render() {
+
+    const { posts } = this.state;
+
+    const Postlist = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card center" key={post.id}>
             <div className="card-content">
-              Hello
+            <h6 className="card-title">{post.title}</h6>
+            <p>{post.body}</p>
             </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="loader"></div>
+    )
+
+
+
+    return (
+
+      <div className="post card center Element">
+        <div className="card-content">
+          {Postlist}
+        </div>
       </div>
 
     )
+  }
 }
 
 export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Link,NavLink} from 'react-router-dom'
-
+import {Link,NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
             <nav>
-                <div class="nav-wrapper orange">
-                    <a href="#" class="brand-logo">Blog</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <div className="nav-wrapper orange">
+                    <a href="#" className="brand-logo ">Blog</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down ">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                         <li><Link to="/post">Post</Link></li>
